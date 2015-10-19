0c2fb4334d32341a740068ce4fc794a4e149531f
Darkkyelfo <raulpedrouag@gmail.com>
2015-10-19 17:26:10 -0200

    inimigo segue o player

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -17,12 +17,14 @@ public class EnemyBehavior : MonoBehaviour {
 	private HP_Bar barraHP;
 	private PlayerBehavior player;//variavel que guarda o jogador
 	private Estado_Do_Inimigo estadoAtual;
+	private float posicaoDir;
 	// Use this for initialization
 	void Start () {
 		status = objStatus.GetComponent ("Status") as Status;
 		barraHP = objBarraHP.GetComponent ("HP_Bar") as HP_Bar;
 		player = FindObjectOfType (typeof(PlayerBehavior)) as PlayerBehavior;
 		setEstado (Estado_Do_Inimigo.idle);
+
 	}
 	
 	// Update is called once per frame
@@ -39,11 +41,11 @@ public class EnemyBehavior : MonoBehaviour {
 		switch (estadoAtual) {
 			case Estado_Do_Inimigo.idle:{
 			float distanciaDoPlayer = Vector3.Distance(transform.position,player.transform.position);//pega a distancia do inimigo para o jogador
-			if(distanciaDoPlayer>=0){
-				transform.Translate(Vector3.left*velocidade*Time.deltaTime);
+			if(distanciaDoPlayer < 100 && distanciaDoPlayer > 0){
+				seguirPlayer(true);
 			}
 			else{
-				transform.Translate(Vector3.right*velocidade*Time.deltaTime);
+				seguirPlayer(false);
 			}
 			}break;
 			case Estado_Do_Inimigo.attack:{
@@ -58,6 +60,18 @@ public class EnemyBehavior : MonoBehaviour {
 
 	void setEstado(Estado_Do_Inimigo novoEstado){
 		estadoAtual = novoEstado;
+	}
+
+	void seguirPlayer(bool seguir){
+		if (player.gameObject.transform.position.x < transform.position.x) {
+			transform.Translate(Vector3.left*velocidade*Time.deltaTime);
+			transform.localScale=new Vector3(2,2,2);//faz o inimigo olhar na direçao do player
+		} else if (player.gameObject.transform.position.x > transform.position.x) {
+			transform.localScale=new Vector3(-2,2,2);
+			transform.Translate(Vector3.right*velocidade*Time.deltaTime);
+		}
+
+
 	}
 
 
